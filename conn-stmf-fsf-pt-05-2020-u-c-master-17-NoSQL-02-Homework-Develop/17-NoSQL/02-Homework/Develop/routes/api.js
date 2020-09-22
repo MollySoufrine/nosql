d2061890483f2def8e5a3631558172a5cca847a3
molly soufrine <mollysoufrine@gmail.com>
2020-09-22 15:32:23 -0400

    started to write my routes in api.js, wrote my get route to find the workouts

--- conn-stmf-fsf-pt-05-2020-u-c-master-17-NoSQL-02-Homework-Develop/17-NoSQL/02-Homework/Develop/routes/api.js
+++ conn-stmf-fsf-pt-05-2020-u-c-master-17-NoSQL-02-Homework-Develop/17-NoSQL/02-Homework/Develop/routes/api.js
@@ -1,7 +1,15 @@
-const router = require('express').Router();
-const Workout = require('../models/workout.js');
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
 // your routes here
-
+app.get("/api/workouts", (req, res) => {
+  Workout.find()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = router;
