4a649ee27a3b5767c9fac17ca38fb6e71da2a885
molly soufrine <mollysoufrine@gmail.com>
2020-09-22 15:10:33 -0400

    Added some information to the schema in workout.js file such as type of exercise, name, duration etc and their types

--- conn-stmf-fsf-pt-05-2020-u-c-master-17-NoSQL-02-Homework-Develop/17-NoSQL/02-Homework/Develop/models/workout.js
+++ conn-stmf-fsf-pt-05-2020-u-c-master-17-NoSQL-02-Homework-Develop/17-NoSQL/02-Homework/Develop/models/workout.js
@@ -1,15 +1,47 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const workoutSchema = new Schema(
   {
-    day: {},
-    exercises: [],
+    day: {
+      type: Date,
+      default: () => new Date(),
+    },
+    exercises: [
+      {
+        type: {
+          type: String,
+          trim: true,
+          required: "Please select an exercise",
+        },
+        name: {
+          type: String,
+          trim: true,
+          required: "please enter a name for your exercise",
+        },
+        duration: {
+          type: Number,
+          required: "How long did you work out?",
+        },
+        weight: {
+          type: Number,
+        },
+        reps: {
+          type: Number,
+        },
+        sets: {
+          type: Number,
+        },
+        distance: {
+          type: Number,
+        },
+      },
+    ],
   },
   {}
 );
 
-const Workout = mongoose.model('Workout', workoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
 
 module.exports = Workout;
