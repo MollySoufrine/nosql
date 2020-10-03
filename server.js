fb5567fa657a78371d6862b166be5f6d70bcbed4
molly soufrine <mollysoufrine@gmail.com>
2020-10-03 16:52:13 -0400

    put server.js connection to mongo back to what we were given

--- server.js
+++ server.js
@@ -13,15 +13,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/fitnesstracker",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  }
-);
+mongoose.connect("mongodb://localhost/fitnesstracker", {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+});
 
 // routes
 app.use(require("./routes/api.js"));
